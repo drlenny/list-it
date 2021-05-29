8ce41ae9b7a0fb5f6bcce21dabb777e4e59e72f9
drlenny <66442932+drlenny@users.noreply.github.com>
2021-05-29 14:25:08 -0500

    fix error in Items model

--- models/items.js
+++ models/items.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Items.belongsTo(models.User, {
+      Items.belongsTo(models.List, {
         foreignKey: 'listId',
         onDelete: 'CASCADE'
       })
